1a2a1c51079f96cefde76a34a1a0189bdbeed12b
BuruDAnette <brunacastromorais@gmail.com>
2025-05-26 15:15:35 -0300

    Initial commit - model

---
+++ prova-20223006782/src/main/java/com/cefet/prova_20223006782/Prova20223006782Application.java
@@ -0,0 +1,13 @@
+package com.cefet.prova_20223006782;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prova20223006782Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prova20223006782Application.class, args);
+	}
+
+}
---
+++ prova-20223006782/src/test/java/com/cefet/prova_20223006782/Prova20223006782ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cefet.prova_20223006782;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prova20223006782ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
